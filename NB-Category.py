dcf94d5cb6a7e85e0e456b55883dd07542c88b39
SandraPK <127471626+SandraPK@users.noreply.github.com>
2023-04-29 19:35:11 +0530

    Update NB-Category.py

--- NB-Category.py
+++ NB-Category.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.naive_bayes import MultinomialNB
@@ -7,20 +8,27 @@ from sklearn.metrics import classification_report
 st.header('Category Prediction')
 
 # Load and preprocess the data
-with open('bbc-text.txt', 'r') as file:
-    data = file.read()
-
-# Split the data into training and testing sets
-X_train, X_test, y_train, y_test = train_test_split(data['text'], data['category'], test_size=0.2, random_state=42)
-
-# Convert the text data into a bag-of-words representation
-vectorizer = CountVectorizer(stop_words='english')
-X_train_vec = vectorizer.fit_transform(X_train)
-X_test_vec = vectorizer.transform(X_test)
-
-# Train a Naive Bayes classifier on the training data
+bbc_text = pd.read_csv(r"C:\Users\HP\Documents\2 nd SEMESTER\NLP CLASS NOTES\bbc-text.txt")
+bbc_text=bbc_text.rename(columns = {'text': 'News_Headline'}, inplace = False)
+bbc_text.head()
+bbc_text.category = bbc_text.category.map({'tech':0, 'business':1, 'sport':2, 'entertainment':3, 'politics':4})
+bbc_text.category.unique()
+X = bbc_text.News_Headline
+y = bbc_text.category
+#split data
+X_train, X_test, y_train, y_test = train_test_split(X, y, train_size = 0.6, random_state = 1)
+vector = CountVectorizer(stop_words = 'english',lowercase=False)
+# fit the vectorizer on the training data
+vector.fit(X_train)
+vector.vocabulary_
+X_transformed = vector.transform(X_train)
+X_transformed.toarray()
+# for test data
+X_test_transformed = vector.transform(X_test)
 naivebayes = MultinomialNB()
-naivebayes.fit(X_train_vec, y_train)
+naivebayes.fit(X_transformed, y_train)
+
+
 
 # Get user input and make a prediction
 input = st.text_area("Enter the text", value="")
