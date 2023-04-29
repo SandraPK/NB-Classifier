974ca9347cb73c171ccff6b8a845dc55832ee397
SandraPK <127471626+SandraPK@users.noreply.github.com>
2023-04-29 19:49:16 +0530

    Update NB-Category.py

--- NB-Category.py
+++ NB-Category.py
@@ -1,12 +1,39 @@
-import pickle
-saved_model = pickle.dumps(naivebayes)
-s = pickle.loads(saved_model)
+import streamlit as st
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.metrics import classification_report
+
+st.header('Category Prediction')
+
+# Load and preprocess the data
+bbc_text = pd.read_csv(r"bbc-text.txt")
+bbc_text=bbc_text.rename(columns = {'text': 'News_Headline'}, inplace = False)
+bbc_text.head()
+bbc_text.category = bbc_text.category.map({'tech':0, 'business':1, 'sport':2, 'entertainment':3, 'politics':4})
+bbc_text.category.unique()
+X = bbc_text.News_Headline
+y = bbc_text.category
+#split data
+X_train, X_test, y_train, y_test = train_test_split(X, y, train_size = 0.6, random_state = 1)
+vector = CountVectorizer(stop_words = 'english',lowercase=False)
+# fit the vectorizer on the training data
+vector.fit(X_train)
+vector.vocabulary_
+X_transformed = vector.transform(X_train)
+X_transformed.toarray()
+# for test data
+X_test_transformed = vector.transform(X_test)
+naivebayes = MultinomialNB()
+naivebayes.fit(X_transformed, y_train)
+
 
 
 # Get user input and make a prediction
 input = st.text_area("Enter the text", value="")
 if st.button("Predict"):
-    vec = vector.transform(input).toarray()
-    pred = s.predict(vec)[0]
+    vec = vectorizer.transform([input]).toarray()
+    pred = naivebayes.predict(vec)[0]
     category = ['TECH', 'BUSINESS', 'SPORTS', 'ENTERTAINMENT', 'POLITICS'][pred]
     st.write("The predicted category is:", category)
